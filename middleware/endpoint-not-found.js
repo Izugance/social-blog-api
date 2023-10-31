4268e931e11fd97f96aa080a95e5fb63a1c50f83
Praise Izuagie <izuagiepraise@gmail.com>
2023-10-31 22:50:08 +0100

    Fix error message

--- middleware/endpoint-not-found.js
+++ middleware/endpoint-not-found.js
@@ -3,7 +3,7 @@ import { StatusCodes } from "http-status-codes";
 export default (req, res, next) => {
   return res.status(StatusCodes.NOT_FOUND).json({
     msg:
-      "Requested endpoint does not exist. Check if you're using the" +
-      "required method or mispelled the URL",
+      "Requested endpoint does not exist. Check if you're using the " +
+      "required method or have mispelled the URL",
   });
 };
